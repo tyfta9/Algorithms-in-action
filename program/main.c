b677b7b20276acf11d318b5793ff14f2a526a7bd
Denys <lazar2den@gmail.com>
2025-03-28 21:43:00 +0000

    add structure product

--- program/main.c
+++ program/main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
+// date length
+#define DATELEN 9
+// name length
+#define NAMELEN 30
+// target engine code length
+#define ENGINECODELEN 6
+
+// Line Code,Batch Code,Batch Date & Time,Product ID,Product Name,Target Engine Code,Bin#,Weight
+struct product 
+{
+    // line code
+    unsigned short line;
+    // batch code
+    unsigned int batch;
+    // product ID
+    unsigned short id;
+    // bin number
+    unsigned short bin;
+    // product weight
+    float weight;
+    // batch date & time
+    char date[DATELEN];
+    // product name
+    char name[NAMELEN];
+    // target engine code
+    char engine[ENGINECODELEN];   
+};
+
+
 int main(void)
 {
     return 0;
