5429be4aff81ea742e2ad3e44cff7b3dcb859505
Denys <lazar2den@gmail.com>
2025-04-18 14:20:07 +0100

    include CarPartProduct.h to other header file

--- include/ReadProductFile.h
+++ include/ReadProductFile.h
@@ -2,12 +2,13 @@
 #if !defined(READPRODUCTFILE_H)
 #define READPRODUCTFILE_H
 
+#include "CarPartProduct.h"
 // public functions of a ReadProductFile.c file
 
 // get total product or line count in all files excluding headers
 int FGetProductCount();
 
 // read all product from file
-void FReadProduct(struct carPartProduct* products, int size);
+void FReadProduct(part* products, int size);
 
 #endif
